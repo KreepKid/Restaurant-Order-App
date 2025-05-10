ba81c892b8b738a4ebc02bc8a1d0cf1308087a10
KreepKid <thabisomahlangu909@gmail.com>
2025-05-10 14:49:40 +0200

    Restaurant Order App

--- app/src/main/java/com/example/restaurantorderapp/MainActivity.java
+++ app/src/main/java/com/example/restaurantorderapp/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.restaurantorderapp;
 
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ConstraintLayout home = (ConstraintLayout) findViewById(R.id.linearLayout0);
+        //setContentView(R.id.linearLayout0);
+
+        Button sign = (Button) findViewById(R.id.signInButton);
+
+        sign.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.user);
+
+                ImageView proceed = (ImageView) findViewById(R.id.imageView11);
+
+                proceed.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                        setContentView(R.layout.post_user);
+
+                    }
+                });
+            }
+        });
+
+
+
     }
 }
