62fb18a78f025037e1face027f53057bd0d7207c
Gomolemo Rampa <wisemanrampa@gmail.com>
2025-05-28 18:00:10 +0200

    Second Ever Commit and Push

---
+++ app/src/main/java/com/example/restaurantorderapp/LoginWelcome.java
@@ -0,0 +1,29 @@
+package com.example.restaurantorderapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.content.SharedPreferences;
+import android.widget.TextView;
+import android.content.Context;
+
+public class LoginWelcome extends AppCompatActivity {
+    TextView name,surname;
+    SharedPreferences preferences;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.loggin_welcome);
+
+        name = (TextView) findViewById(R.id.textCustomerName);
+        surname = (TextView) findViewById(R.id.textCustomerSurname);
+        preferences =  this.getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
+
+        String custName = preferences.getString("name","Error, name not found");
+        String custSurname = preferences.getString("surname","Error, surname not found");
+
+        name.setText(custName);
+        surname.setText(custSurname);
+    }
+}
