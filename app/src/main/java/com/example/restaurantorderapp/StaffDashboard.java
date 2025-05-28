2ac0da3865a4191a9d6843167828441a3d31b09d
Gomolemo Rampa <wisemanrampa@gmail.com>
2025-05-28 20:39:37 +0200

    Uhmm, we moving.

--- app/src/main/java/com/example/restaurantorderapp/StaffDashboard.java
+++ app/src/main/java/com/example/restaurantorderapp/StaffDashboard.java
@@ -1,19 +1,16 @@
 package com.example.restaurantorderapp;
 
 import android.content.Intent;
-import android.support.constraint.ConstraintLayout;
-import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
-public class StaffHomepage extends AppCompatActivity {
+public class StaffDashboard extends AppCompatActivity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.staff_home);
+        setContentView(R.layout.staff_dashboard);
 
         TextView staffProfile = (TextView) findViewById(R.id.staffProfile);
         TextView createOrder = (TextView) findViewById(R.id.staffCreateOrder);
--- app/src/main/java/com/example/restaurantorderapp/StaffLogin.java
+++ app/src/main/java/com/example/restaurantorderapp/StaffLogin.java
@@ -1,16 +1,11 @@
 package com.example.restaurantorderapp;
 
 import android.content.Intent;
-import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
 public class StaffLogin extends AppCompatActivity {
 
@@ -26,7 +21,7 @@ public class StaffLogin extends AppCompatActivity {
         staffLoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), StaffHomepage.class);
+                Intent intent = new Intent(getApplicationContext(), StaffDashboard.class);
                 startActivity(intent);
             }
         });
